4189d54e8e0d15d1f244231f5dd7b1f709bb2d88
DeepfriedWafflez <46905184+DeepfriedWafflez@users.noreply.github.com>
2019-05-14 14:41:00 -0600

    DB Connect class setup

--- src/DLL/DBConnect.java
+++ src/DLL/DBConnect.java
@@ -1,4 +1,27 @@
 package DLL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnect {
+
+    public static Connection getConnection()
+    {
+        try {
+            //gets driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //connection string properties
+            String url = "jdbc:mysql://localhost:3306/travelexperts";
+            String username = "root";
+            String password = null;
+
+            //returns connection
+            return DriverManager.getConnection(url, username, password);
+
+        } catch (ClassNotFoundException | SQLException e) { e.printStackTrace();}
+
+        return null;
+    }
 }
