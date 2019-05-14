da4470d9a7293e833c892292556d946f9bb54d08
DeepfriedWafflez <46905184+DeepfriedWafflez@users.noreply.github.com>
2019-05-14 14:19:32 -0600

    Agent object complete

--- src/BLL/Agent.java
+++ src/BLL/Agent.java
@@ -1,6 +1,7 @@
 package BLL;
 
 public class Agent {
+    //properties
     private int id;
     private simpleStringProperty firstName;
     private simpleStringProperty middleInitial;
@@ -10,12 +11,13 @@ public class Agent {
     private simpleStringProperty position;
     private int agency;
 
-    public Agent(int id, simpleStringProperty firstName, simpleStringProperty middleInitial, simpleStringProperty lastName,
+    //constructor
+    public Agent(int id, simpleStringProperty fName, simpleStringProperty mInitial, simpleStringProperty lName,
                  simpleStringProperty phone, simpleStringProperty email, simpleStringProperty position, int agency) {
-        id = id;
-        this.firstName = firstName;
-        this.middleInitial = middleInitial;
-        this.lastName = lastName;
+        this.id = id;
+        this.firstName = fName;
+        this.middleInitial = mInitial;
+        this.lastName = lName;
         this.phone = phone;
         this.email = email;
         this.position = position;
@@ -31,5 +33,12 @@ public class Agent {
     public String getEmail(){ return email.get(); }
     public String getPosition(){ return position.get(); }
 
-
+    //Set methods -- ID is auto set by database
+    public void setFirstName(string fname){ firstName.set(fname); }
+    public void setMiddleInitial(string mInit){ middleInitial.set(mInit); }
+    public void setLastName(string lname){ lastName.set(lname); }
+    public void setPhone(string phoneNew){ phone.set(phoneNew); }
+    public void setEmail(string emailNew){ email.set(emailNew); }
+    public void setPosition(string posit){ position.set(posit); }
+    public void setAgency(int agcy){ agency.set(agcy); }
 }
