3b2490525e2eeed7f9e2daddb7820356c6930efe
DeepfriedWafflez <46905184+DeepfriedWafflez@users.noreply.github.com>
2019-05-15 15:08:20 -0600

    Customer, CustomerDB
    Finished Customer Class, added get/update functions for customer db

--- src/BLL/Customer.java
+++ src/BLL/Customer.java
@@ -1,4 +1,63 @@
 package BLL;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Customer {
+    //properties
+    private int id;
+    private SimpleStringProperty firstName;
+    private SimpleStringProperty lastName;
+    private SimpleStringProperty address;
+    private SimpleStringProperty city;
+    private SimpleStringProperty province;
+    private SimpleStringProperty postal;
+    private SimpleStringProperty country;
+    private SimpleStringProperty homePhone;
+    private SimpleStringProperty busPhone;
+    private SimpleStringProperty email;
+    private int agent;
+
+    //constructor
+    public Customer(int id, String fname, String lname, String address, String city, String prov, String postal,
+                    String country, String hPhone, String bPhone, String email, int agentId) {
+        this.id = id;
+        this.firstName = new SimpleStringProperty(fname);
+        this.lastName = new SimpleStringProperty(lname);
+        this.address = new SimpleStringProperty(address);
+        this.city = new SimpleStringProperty(city);
+        this.province = new SimpleStringProperty(prov);
+        this.postal = new SimpleStringProperty(postal);
+        this.country = new SimpleStringProperty(country);
+        this.homePhone = new SimpleStringProperty(hPhone);
+        this.busPhone = new SimpleStringProperty(bPhone);
+
+    }
+
+    //Get methods
+    public int getId(){ return id; }
+    public String getFirstName(){ return firstName.get(); }
+    public String getLastName(){ return firstName.get(); }
+    public String getAddress(){ return address.get(); }
+    public String getCity(){ return city.get(); }
+    public String getProv(){ return province.get(); }
+    public String getPostal(){ return postal.get(); }
+    public String getCountry(){ return country.get(); }
+    public String getHomePhone(){ return homePhone.get(); }
+    public String getBusPhone(){ return busPhone.get(); }
+    public String getEmail(){ return email.get(); }
+    public int getAgent(){ return agent; }
+
+
+    //Set methods -- Customer Id is auto set by database
+    public void setFirstName(String fname){ firstName.set(fname); }
+    public void setLastName(String lname) { lastName.set(lname); }
+    public void setAddress(String add) { address.set(add); }
+    public void setCity(String cty) { city.set(cty); }
+    public void setProv(String prov) { province.set(prov); }
+    public void setPostal(String post) { postal.set(post); }
+    public void setCountry(String cty) { country.set(cty); }
+    public void setHomePhone(String phone) { homePhone.set(phone); }
+    public void setBusPhone(String phone) { busPhone.set(phone); }
+    public void setEmail(String eml) { email.set(eml); }
+    public void setAgent(int id) { agent = id; }
 }
--- src/BLL/CustomerDB.java
+++ src/BLL/CustomerDB.java
@@ -1,4 +1,100 @@
 package BLL;
 
+import DLL.DBConnect;
+import javafx.scene.control.Alert;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerDB {
+
+    //gets list of customers
+    public static List<Customer> getCustomers(){
+        List<Customer> customers = null;
+
+        try{
+            //connection built
+            Connection connect = DBConnect.getConnection();
+
+            //query
+            String selectQuery = "select CustomerId, CustFirstName, CustLastName, CustAddress, CustCity, CustProv," +
+                                    "CustPostal, CustCountry, CustHomePhone, CustBusPhone, CustEmail from Customers";
+
+            //makes a sql statement
+            Statement query = connect.createStatement();
+
+            //assigns & executes statement
+            ResultSet rs = query.executeQuery(selectQuery);
+
+            customers = new ArrayList<Customer>();
+            //runs while reader has data
+            while(rs.next()){
+                Customer customer = new Customer(rs.getInt("CustomerId"),
+                        rs.getString("CustFirstName"),
+                        rs.getString("CustLastName"),
+                        rs.getString("CustAddress"),
+                        rs.getString("CustCity"),
+                        rs.getString("CustProv"),
+                        rs.getString("CustPostal"),
+                        rs.getString("CustCountry"),
+                        rs.getString("CustHomePhone"),
+                        rs.getString("CustBusPhone"),
+                        rs.getString("CustEmail"),
+                        rs.getInt("AgentId"));
+                customers.add(customer);
+            }
+            connect.close();
+
+        }catch(Exception e) { e.printStackTrace(); }
+
+        return customers;
+    }
+
+    //updates a customer
+    public static void updateCustomer(Customer customer){
+        try{
+            //connection built
+            Connection connect = DBConnect.getConnection();
+
+            //query
+            String updateQuery = "update Customers set CustFirstName=?, CustLastName=?, CustAddress=?, CustCity=?, CustProv=?," +
+                    "CustPostal=?, CustCountry=?, CustHomePhone=?, CustBusPhone=?, CustEmail=?, AgentId=? where CustomerId=?";
+
+            PreparedStatement stmt = connect.prepareStatement(updateQuery);
+
+            //sets parameters for ?
+            stmt.setString(1, customer.getFirstName());
+            stmt.setString(2, customer.getLastName());
+            stmt.setString(3, customer.getAddress());
+            stmt.setString(4, customer.getCity());
+            stmt.setString(5, customer.getProv());
+            stmt.setString(6, customer.getPostal());
+            stmt.setString(7, customer.getCountry());
+            stmt.setString(8, customer.getHomePhone());
+            stmt.setString(9, customer.getBusPhone());
+            stmt.setString(10, customer.getEmail());
+            stmt.setInt(11, customer.getAgent());
+            stmt.setInt(12, customer.getId());
+
+            //checks if the data was inserted
+            int numRows = stmt.executeUpdate();
+            if (numRows == 0)
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Customer failed to update. Contact Tech Support.");
+                alert.showAndWait();
+            }
+            connect.close();
+
+        }catch(Exception e) { e.printStackTrace(); }
+    }
+
+    //deletes a customer
+    public static void deleteCustomer(){
+
+    }
+
 }
