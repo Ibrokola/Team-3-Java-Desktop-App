a5baddd99ba65050f7b5db6d3c0c36169fa3731c
Guido Amaya Robayo <812406@ACDM.DS.SAIT.CA>
2019-05-31 15:21:18 -0600

    packages ongoing
    still working on  button actions

--- src/app/PackageController.java
+++ src/app/PackageController.java
@@ -1,14 +1,212 @@
 package app;
 
+import BLL.Administrator;
 import BLL.Package;
-import BLL.PackageDB;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-import javax.swing.text.TabableView;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class PackageController {
+    /*
+     * Purpose: To act as a base Controller for all other pages to branch from.
+     * Author: Brent Ward - starting template found at k33ptoo.
+     * Module: PROJ-207-OSD
+     * Date: May 28, 2019
+     * */
 
+    //buttons
+    @FXML private Button btnDashboard;
+    @FXML private Button btnAgents;
+    @FXML private Button btnCustomers;
+    @FXML private Button btnPackages;
+    @FXML private Button btnProducts;
+    @FXML private Button btnSuppliers;
+    @FXML private Button btnSettings;
+    @FXML private Button btnSignout;
+    @FXML private Button btnAdd;
+    @FXML private Button btnUpdate;
+    @FXML private Button btnDelete;
+
+    //panes
+    @FXML private Pane paneAdd;
+    @FXML private Pane paneUpdate;
+    @FXML private Pane paneOverview;
+
+    //Other properties
+    @FXML private Label lblUserName;
+    @FXML private Label lblClock;
+    @FXML private TextField txtSearch;
+
+
+    //handles all button clocks
+    @FXML void handleButtonClicks(ActionEvent event) throws IOException {
+        //dashboard button
+        if(event.getSource() == btnDashboard){
+            //Changes the scene, fetches the stage
+            Parent root = FXMLLoader.load(getClass().getResource("../views/dashboard.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //grabs the stage
+            stage.setScene(scene);
+        }
+        //agent button
+        if(event.getSource() == btnAgents){
+            //Changes the scene, fetches the stage
+            Parent root = FXMLLoader.load(getClass().getResource("../views/agent.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //grabs the stage
+            stage.setScene(scene);
+        }
+        //customer button
+        if(event.getSource() == btnCustomers){
+            //Changes the scene, fetches the stage
+            Parent root = FXMLLoader.load(getClass().getResource("../views/customer.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //grabs the stage
+            stage.setScene(scene);
+        }
+        //package button
+        if(event.getSource() == btnPackages){
+            //Changes the scene, fetches the stage
+            Parent root = FXMLLoader.load(getClass().getResource("../views/package.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //grabs the stage
+            stage.setScene(scene);
+        }
+        //product button
+        if(event.getSource() == btnProducts){
+            //Changes the scene, fetches the stage
+            Parent root = FXMLLoader.load(getClass().getResource("../views/product.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //grabs the stage
+            stage.setScene(scene);
+        }
+        //supplier button
+        if(event.getSource() == btnSuppliers){
+            //Changes the scene, fetches the stage
+            Parent root = FXMLLoader.load(getClass().getResource("../views/supplier.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //grabs the stage
+            stage.setScene(scene);
+        }
+        //settings button
+        if(event.getSource() == btnSettings){
+            //Changes the scene, fetches the stage
+            Parent root = FXMLLoader.load(getClass().getResource("../views/settings.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //grabs the stage
+            stage.setScene(scene);
+        }
+        //log out
+        if(event.getSource() == btnSignout){
+            //Changes the scene, fetches the stage
+            Parent root = FXMLLoader.load(getClass().getResource("../views/login.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow(); //grabs the stage
+            stage.setScene(scene);
+        }
+
+        /*** Pane switching buttons ***/
+        if(event.getSource() == btnAdd){
+            paneAdd.toFront();
+        }
+        if(event.getSource() == btnUpdate){
+            paneUpdate.toFront();
+        }
+
+        /*** Operational buttons ***/
+        if(event.getSource() == btnDelete){ }
+
+    }
+
+    //Widget Code
+    private void startClock() {
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss a   -   dd/MM/YYYY");
+            lblClock.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+
+    //Startup Sequence
+    @FXML void initialize() {
+        startClock(); //runs the clock
+        //builds the welcome label
+        Administrator user = LoginController.userLoggedIn();
+        lblUserName.setText(user.getLastName() + ", " + user.getFirstName());
+        lblUserName.setWrapText(true);
+    }
+
+    //Table view for Packages data
     @FXML
     private TableView<Package> tblPackages;
+
+    //Columns to display data from packages table
+    @FXML
+    private TableColumn<Package, Integer> colPkgId;
+    @FXML
+    private TableColumn<Package, String> colPkgName;
+    @FXML
+    private TableColumn<Package, Date> colPkgStrartDate;
+    @FXML
+    private TableColumn<Package, Date> colPkgEndDate;
+    @FXML
+    private TableColumn<Package, Double> colPkgBasePrice;
+    @FXML
+    private TableColumn<Package, String> colPkgDescription;
+
+    //Header for Packages Panel
+    @FXML
+    private Label lblHeadPackages;
+
+
+    //@FXML
+    //private Pane paneAdd;
+
+    //Form's fields to be filled in order to add a Package to "packages" table
+    @FXML
+    private TextField txtAddPkgName;
+    @FXML
+    private DatePicker dpPkgStartDate;
+    @FXML
+    private DatePicker dpPkgEndDate;
+    @FXML
+    private TextField txtPkgDescription;
+    @FXML
+    private TextField txtPkgBasePrice;
+
+
+    /*** Buttons ****/
+    //Button for adding input into "packages" table
+    @FXML
+    private Button btnAddPackage;
+
+    //button to go back to main panel
+    @FXML
+    private Button btnAddGoBack;
+
+
+    /*** Methods for Buttons ****/
+     //method for Add button
+    void btnAddPackage(){
+
+    }
+
+
+
 }
